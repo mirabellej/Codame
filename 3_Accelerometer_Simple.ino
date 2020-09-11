c8e08d52c35537df3fa509e723b7e986118c36a1
mirabellej <mirabellejones@gmail.com>
2020-09-11 20:17:54 +0200

    Add files via upload

---
+++ 3_Accelerometer_Simple.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_CircuitPlayground.h>
+
+float X, Y, Z;
+
+void setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+}
+
+void loop() {
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+
+  
+  Serial.println(X);
+  Serial.println(Y);
+  Serial.println(Z);
+
+  delay(500);
+}
